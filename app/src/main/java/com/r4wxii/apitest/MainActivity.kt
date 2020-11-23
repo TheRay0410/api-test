1fed8cfa02e4149b6a628cd506a9941beeb75d6d
TheRay0410 <yn041010@gmail.com>
2020-11-23 13:07:36 +0900

    initial commit

---
+++ app/src/main/java/com/r4wxii/apitest/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.r4wxii.apitest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.r4wxii.apitest.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/r4wxii/apitest/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.r4wxii.apitest.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
