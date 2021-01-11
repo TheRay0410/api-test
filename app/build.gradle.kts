1479ba2dbf600b5e8f465e54ee9adac4c8b61b1a
TheRay0410 <yn041010@gmail.com>
2021-01-11 21:52:21 +0900

    update Kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     kotlin("android")
     kotlin("kapt")
     id("dagger.hilt.android.plugin")
-    kotlin("plugin.serialization") version "1.4.10"
+    kotlin("plugin.serialization") version "1.4.20"
     id("androidx.navigation.safeargs.kotlin")
 }
 
@@ -46,7 +46,7 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.10")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.20")
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.2.1")
